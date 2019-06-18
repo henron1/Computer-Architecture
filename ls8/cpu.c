f3f2d2897031f36ed39d77bcc1f68c632701decf
Henry Neal <henry.neal0@gmail.com>
2019-06-18 18:59:26 -0400

    day 1 mvp done

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -2,24 +2,35 @@
 
 #define DATA_LEN 6
 
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index)
+{
+  return cpu->ram[index];
+}
+
+unsigned char cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char value)
+{
+  return cpu->ram[index] = value;
+}
+
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
 void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+      // From print8.ls8
+      0b10000010, // LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, // PRN R0
+      0b00000000,
+      0b00000001 // HLT
   };
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
+  for (int i = 0; i < DATA_LEN; i++)
+  {
     cpu->ram[address++] = data[i];
   }
 
@@ -31,10 +42,11 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    // TODO
+    break;
 
     // TODO: implement more ALU ops
   }
@@ -47,7 +59,8 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  while (running) {
+  while (running)
+  {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. Figure out how many operands this next instruction requires
@@ -55,6 +68,33 @@ void cpu_run(struct cpu *cpu)
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
+
+    int running = 1;
+    while (running)
+    {
+      unsigned char IR = cpu_ram_read(cpu, cpu->pc); // instruction address in program counter
+      unsigned char operandA = cpu_ram_read(cpu, cpu->pc + 1);
+      unsigned char operandB = cpu_ram_read(cpu, cpu->pc + 2);
+      switch (IR)
+      {
+      default:
+        break;
+
+      // 5. Do whatever the instruction should do according to the spec.
+      case HLT:
+        running = 0;
+        exit(1);
+        break;
+
+      case LDI:
+        cpu->reg[operandA] = operandB;
+        break;
+
+      case PRN:
+        printf("%d\n", cpu->reg[operandA]);
+        break;
+      }
+    }
   }
 }
 
@@ -64,4 +104,6 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  memset(cpu->ram, 0, sizeof cpu->ram);
+  memset(cpu->reg, 0, sizeof cpu->reg);
 }
